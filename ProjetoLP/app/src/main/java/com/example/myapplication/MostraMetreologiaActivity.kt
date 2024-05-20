ff38419ad33f39928e2ad706ffad255e7816505b
Gustavo123432 <mickeymoney333@gmail.com>
2024-05-20 21:29:09 +0100

    Update

--- ProjetoLP/app/src/main/java/com/example/myapplication/MostraMetreologiaActivity.kt
+++ ProjetoLP/app/src/main/java/com/example/myapplication/MostraMetreologiaActivity.kt
@@ -1,13 +1,18 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,14 +22,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.api.Endpoint
 import com.example.myapplication.models.TempoInformation
 import com.example.myapplication.util.NetworkUtils
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Response
+import kotlin.math.log
 
-class MostraMetreologiaActivity : AppCompatActivity() {
+class MostraMetreologiaActivity : AppCompatActivity(), CustomAdapterTempo.RecyclerViewEvent {
+    //add only to click CustomAdapterTempo.RecyclerViewEvent
     private lateinit var testSpinner : Spinner
     private lateinit var recyclerView: RecyclerView
     private lateinit var progressBar: ProgressBar
+    private lateinit var sharedPreferences : SharedPreferences
+    private lateinit var floatingActionButton: FloatingActionButton
 
     var districtGlobalIds = mutableMapOf<String, Int>()
     var tempoInfomationn = ArrayList<TempoInformation>()
@@ -49,13 +59,19 @@ class MostraMetreologiaActivity : AppCompatActivity() {
         testSpinner = findViewById(R.id.spinnerTest)
         recyclerView = findViewById(R.id.recyclerView)
         progressBar = findViewById(R.id.progressBar)
-        getCurrencies()
+        floatingActionButton = findViewById(R.id.floatingActionButton)
+        getCurrencies(this)
 
         /*aparecer widgets com os 3 locais e o tempo previsto para esse dia, salvar o ultimo local escolhido shared preferences
         * logout sai da conta fazer o registar activity*/
 
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this, test::class.java)
+            startActivity(intent)
+        }
+
     }
-    fun getCurrencies (){
+    fun getCurrencies (context: Context){
         val retrofitClient = NetworkUtils.getRetrofitInstance("https://api.ipma.pt/")
         val endpoint = retrofitClient.create(Endpoint::class.java)
 
@@ -91,11 +107,11 @@ class MostraMetreologiaActivity : AppCompatActivity() {
                                 globalId = districtGlobalIds[selectedDistrict].toString()
 
                                 tempoInfomationn.clear()
-                                val customAdapterTempo = CustomAdapterTempo(tempoInfomationn, this@MostraMetreologiaActivity)
+                                val customAdapterTempo = CustomAdapterTempo(tempoInfomationn,this@MostraMetreologiaActivity)
                                 recyclerView.layoutManager = LinearLayoutManager(this@MostraMetreologiaActivity, LinearLayoutManager.HORIZONTAL, false)
                                 recyclerView.adapter = customAdapterTempo
                                 progressBar()
-                                weatherUpdate()
+                                weatherUpdate(this@MostraMetreologiaActivity)
                                 // Faça o que for necessário com o ID global
                                 // Por exemplo, armazene-o em uma variável ou passe-o para outra função
                                 Log.d("Selected District", "Name: $selectedDistrict, Global ID: $globalId")
@@ -121,7 +137,7 @@ class MostraMetreologiaActivity : AppCompatActivity() {
         })
     }
 
-        fun weatherUpdate() {
+        fun weatherUpdate(context: Context) {
 
             val retrofitClient = NetworkUtils.getRetrofitInstance("https://api.ipma.pt/")
             val endpoint = retrofitClient.create(Endpoint::class.java)
@@ -191,8 +207,22 @@ class MostraMetreologiaActivity : AppCompatActivity() {
 
         countDownTimer.start()
     }
+    override fun onItemClick(position: Int) {
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("lala", tempoInfomationn[position].dia)
+        editor.apply()
+        Log.e("Your Tag for identy", tempoInfomationn[position].dia);
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+
+    }
+
+
 
 }
+
 
 private fun <E> ArrayList<E>.add(dia: String?, tempoId: String?, temperaturamMinima: String?, temperaturaMaxima: String?, direcaoVento: String?, precipitacao: String?) {
 
